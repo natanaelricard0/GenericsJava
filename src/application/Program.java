08b40b8a8304aae9c5056a0da33ce78f53952b1e
Natanael Ricardo <nathanshurle@gmail.com>
2022-07-04 10:47:25 -0300

    solução sem default

---
+++ src/application/Program.java
@@ -0,0 +1,31 @@
+package application;
+
+import java.util.Scanner;
+
+import services.PrintService;
+import services.PrintServiceString;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		PrintServiceString ps = new PrintServiceString();
+		
+		System.out.println("How many values? ");
+		int n = sc.nextInt();
+		
+		for (int i = 0; i < n; i++) {
+			String  value = sc.next();
+			ps.addValue(value);
+		}
+		
+		ps.print();
+		
+		System.out.println("First: " + ps.first());
+		
+		sc.close();
+	}
+
+}
